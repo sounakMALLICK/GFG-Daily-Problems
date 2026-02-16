b7e715cb3742d16d0dd657c49027528d8d072540
Sounak Kumar Mallick <150178363+sounakMALLICK@users.noreply.github.com>
2026-02-16 22:08:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.java
@@ -0,0 +1,12 @@
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        // code here
+        Arrays.sort(arr, (a,b) -> a[0]-b[0]);
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i+1][0]<arr[i][1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
